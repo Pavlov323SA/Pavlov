543a91d860317ef7d1429cf7d9e601082e825b48
Pavlov323SA <ilapavlov078@gmail.com>
2025-11-24 20:15:03 +0500

    Create 1.py

---
+++ 1deadline/7LR/1.py
@@ -0,0 +1,12 @@
+direction = input("Введите направление: ")
+
+if direction == "left":
+    print("Иду влево")
+elif direction == "right":
+    print("Иду вправо")
+elif direction == "straight":
+    print("Иду прямо")
+elif direction == "back":
+    print("Иду назад")
+else:
+    print("Неправильное направление")
