d84da58affafbfa4661ea2c853e686971650c215
Pavlov323SA <ilapavlov078@gmail.com>
2025-12-19 09:05:13 +0500

    Add files via upload

---
+++ Deadline 3/LR1/2.py
@@ -0,0 +1,15 @@
+def make_bold(func):
+    def wrapper():
+        result = func()
+        return f"<b>{result}</b>"
+    
+    return wrapper
+
+
+@make_bold
+def get_text():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    print(get_text())
---
+++ Deadline 3/LR1/5.py
@@ -0,0 +1,19 @@
+def safe_exec(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ZeroDivisionError:
+            print("Ошибка: Деление на ноль!")
+            return 0
+    
+    return wrapper
+
+
+@safe_exec
+def divide(a, b):
+    return a / b
+
+
+if __name__ == "__main__":
+    print(f"Результат 1: {divide(10, 2)}")
+    print(f"Результат 2: {divide(5, 0)}")
