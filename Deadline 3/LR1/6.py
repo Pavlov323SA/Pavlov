983e575eac742173a17d3b1ea2baa8bf2d7153c9
Pavlov323SA <ilapavlov078@gmail.com>
2025-12-19 21:13:00 +0500

    Add files via upload

---
+++ Deadline 3/LR1/6.py
@@ -0,0 +1,16 @@
+def repeat(n):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(n):
+                func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+@repeat(3)
+def greet(name):
+    print(f"Привет, {name}!")
+
+
+if __name__ == "__main__":
+    greet("Вася")
